f46d1844bbe42a42e9c2364ad3e8fb0f407f77bf
Annika <annikahall@annikas-macbook-pro.local>
2019-01-11 13:40:24 -0600

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Annika Lynn Nordstrom Hall on 1/11/19.
+//  Copyright © 2019 Annika Hall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
