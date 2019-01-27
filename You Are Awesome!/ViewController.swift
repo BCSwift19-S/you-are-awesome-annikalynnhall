7f789302f0637cc0dd7e5774f83cf8467f58eb91
Annika <annikahall@annikas-macbook-pro.local>
2019-01-27 15:48:32 -0500

    2.17 Completed: Sounds

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var awesomeImageView: UIImageView!
+    var awesomePlayer = AVAudioPlayer()
+    
     var index = -1
     var imageIndex = -1
+    var soundIndex = -1
     let numberOfImages = 10
+    let numberOfSounds = 3
     
     // Code below execute when the app's view first loads
     override func viewDidLoad() {
@@ -35,6 +40,8 @@ class ViewController: UIViewController {
                         "I really like to eat cheese. It's the greatest food every invented."]
         
         var newIndex: Int
+        
+        //SHOW MESSAGE:
         repeat{
             newIndex = Int.random(in: 0..<messages.count)
         } while index == newIndex
@@ -42,12 +49,34 @@ class ViewController: UIViewController {
         index = newIndex
         messageLabel.text = messages[index]
         
+        //SHOW IMAGE:
         repeat {
             newIndex = Int.random(in: 0..<numberOfImages)
         } while imageIndex == newIndex
         
         imageIndex = newIndex
         awesomeImageView.image = UIImage(named: "image\(imageIndex)")
+        
+        //PLAY SOUND:
+        repeat {
+            newIndex = Int.random(in: 0..<numberOfSounds)
+        } while soundIndex == newIndex
+        
+        soundIndex = newIndex
+        
+        var soundName = "sound\(soundIndex)"
+        
+        //Can we load in the file soundName?
+        if let sound = NSDataAsset(name: soundName){
+            do{
+                try awesomePlayer = AVAudioPlayer(data: sound.data)
+                awesomePlayer.play()
+            } catch{
+                print("ERROR: data in \(soundName) couldn't be played as a sound.")
+            }
+        }else{
+            print("ERROR: file \(soundName) didn't load.")
+        }
     }
 
     }
